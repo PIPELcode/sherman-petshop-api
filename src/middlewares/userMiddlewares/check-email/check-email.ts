ade7b59d6d88f03b14cbf1df30c5fbcff2291ae5
Lautaro Valencia <laugabvalencia@gmail.com>
2026-06-30 17:19:50 -0300

    Import dbConnection in check-email middleware

--- src/middlewares/userMiddlewares/check-email/check-email.ts
+++ src/middlewares/userMiddlewares/check-email/check-email.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { UserModel } from "../../../models/user/userModel";
-
+import { dbConnection } from "../../../config/database/dbConnection";
 export const checkEmail = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
 
