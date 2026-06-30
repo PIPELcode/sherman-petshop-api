34a2a4e7d2e9a8cabc0a9125f1afaf916defad7f
Lautaro Gabriel Valencia <laugabvalencia@gmail.com>
2026-06-30 17:48:25 -0300

    dbconnection changes

--- src/config/database/dbConnection.ts
+++ src/config/database/dbConnection.ts
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
+let cachedConnection: typeof mongoose | null = null;
+
 export const dbConnection = async (): Promise<void> => {
-    try {
-        const dbUrl = process.env.MONGODB_URL;
-        if (!dbUrl) {
-
-            throw new Error("MONGODB_URL is not defined in the environment variables.");
-            
-        }
-
-        await mongoose.connect(dbUrl);
-        console.log("Database connected successfully");
-
-    } catch (error) {
-        console.error("Error connecting to the database:", error);
-        throw error;
+    if (cachedConnection) {
+        return;
     }
-}
+
+    const dbUrl = process.env.MONGODB_URL;
+    if (!dbUrl) {
+        throw new Error("MONGODB_URL is not defined.");
+    }
+
+    cachedConnection = await mongoose.connect(dbUrl, {
+        bufferCommands: false,
+    });
+    console.log("Database connected successfully");
+};
